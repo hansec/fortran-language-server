hansec/fortran-language-server

commit a5797dd14edf4d7e3ecd61eb32bfddb3f667fda5

Ignore autocomplete and definition requests in comment lines

fortls/langserver.py
@@ -3,7 +3,8 @@ import os
 import traceback
 import re
 # Local modules
-from fortls.parse_fortran import process_file, read_use_stmt, detect_fixed_format
+from fortls.parse_fortran import process_file, read_use_stmt, detect_fixed_format, \
+    detect_comment_line
 from fortls.objects import find_in_scope, fortran_meth, get_use_tree
 
 log = logging.getLogger(__name__)
@@ -588,18 +589,22 @@ class LangServer:
             # Default context
             return 0, var_prefix, None
         # Get parameters from request
+        req_dict = {"isIncomplete": False, "items": []}
         params = request["params"]
         uri = params["textDocument"]["uri"]
         path = path_from_uri(uri)
         if path not in self.workspace:
-            return {"isIncomplete": False, "items": []}
+            return req_dict
         # Check line
         ac_line = params["position"]["line"]
         ac_char = params["position"]["character"]
         # Get full line (and possible continuations) from file
         curr_line, ac_char = get_line(ac_line, ac_char, self.workspace[path])
         if curr_line is None:
-            return {"isIncomplete": False, "items": []}
+            return req_dict
+        # Ignore for comment lines
+        if detect_comment_line(curr_line):
+            return req_dict
         is_member = False
         try:
             line_prefix = curr_line[:ac_char].lower()
@@ -607,10 +612,9 @@ class LangServer:
             is_member = (len(var_stack) > 1)
             var_prefix = var_stack[-1].strip()
         except:
-            return {"isIncomplete": False, "items": []}
+            return req_dict
         # print var_stack
         file_obj = self.workspace[path]["ast"]
-        test_output = {"isIncomplete": False}
         item_list = []
         scope_list = file_obj.get_scopes(ac_line+1)
         # Get context
@@ -619,7 +623,7 @@ class LangServer:
         line_context, var_prefix, context_info = \
             get_context(line_prefix, var_prefix)
         if var_prefix == '' and not (is_member or line_context == 2):
-            return {"isIncomplete": False, "items": []}
+            return req_dict
         # USE stmnt
         if line_context == 1:  # module part
             for key in self.obj_tree:
@@ -629,8 +633,8 @@ class LangServer:
                     continue
                 if candidate.name.lower().startswith(var_prefix):
                     item_list.append(build_comp(candidate, name_only=True))
-            test_output["items"] = item_list
-            return test_output
+            req_dict["items"] = item_list
+            return req_dict
         elif line_context == 2:  # only part
             name_only = True
             mod_name = context_info.lower()
@@ -676,8 +680,8 @@ class LangServer:
                 continue
             #
             item_list.append(build_comp(candidate, name_only=name_only))
-        test_output["items"] = item_list
-        return test_output
+        req_dict["items"] = item_list
+        return req_dict
 
     def get_definition(self, filepath, def_line, def_char):
         if filepath not in self.workspace:
@@ -685,6 +689,9 @@ class LangServer:
         # Get full line (and possible continuations) from file
         curr_line, def_char = get_line(def_line, def_char, self.workspace[filepath])
         if curr_line is None:
+            return None
+        # Ignore for comment lines
+        if detect_comment_line(curr_line):
             return None
         #
         is_member = False

fortls/parse_fortran.py
@@ -35,12 +35,13 @@ KIND_SPEC_MATCH = re.compile(r'\([a-z0-9_, =*]*\)', re.I)
 SQ_STRING_REGEX = re.compile(r'\'[^\']*\'', re.I)
 DQ_STRING_REGEX = re.compile(r'\"[^\"]*\"', re.I)
 #
-FIXED_COMMENT_LINE_MATCH = re.compile(r'(!|c|d|\*)')
+FIXED_COMMENT_LINE_MATCH = re.compile(r'(!|c|d|\*)', re.I)
 FIXED_CONT_REGEX = re.compile(r'(     [\S])')
 #
 FREE_COMMENT_LINE_MATCH = re.compile(r'([ \t]*!)')
 FREE_CONT_REGEX = re.compile(r'([ \t]*&)')
 FREE_FORMAT_TEST = re.compile(r'[ ]{1,4}[a-z]', re.I)
+OPENMP_LINE_MATCH = re.compile(r'[ \t]*[!|c|\*]\$OMP', re.I)
 
 
 def detect_fixed_format(file_lines):
@@ -58,6 +59,18 @@ def detect_fixed_format(file_lines):
         if len(line_end) > 0 and line_end[-1] == '&':
             return False
     return True
+
+
+def detect_comment_line(line, fixed_format=False):
+    if OPENMP_LINE_MATCH.match(line) is not None:
+        return False
+    if fixed_format:
+        if FIXED_COMMENT_LINE_MATCH.match(line) is not None:
+            return True
+    else:
+        if strip_strings(line).find('!') > -1:
+            return True
+    return False
 
 
 def strip_strings(in_str):

commit 84d6301e01de1dfbcca6a1c177c1fab04e69f45c

Fix bug disabling autcomplete/definition in lines with trailing comments, introduced in a5797dd

fortls/langserver.py
@@ -605,12 +605,12 @@ class LangServer:
         curr_line, ac_char = get_line(ac_line, ac_char, self.workspace[path])
         if curr_line is None:
             return req_dict
-        # Ignore for comment lines
-        if detect_comment_line(curr_line):
-            return req_dict
         is_member = False
         try:
             line_prefix = curr_line[:ac_char].lower()
+            # Ignore for comment lines
+            if detect_comment_line(line_prefix):
+                return req_dict
             var_stack = get_var_stack(line_prefix)
             is_member = (len(var_stack) > 1)
             var_prefix = var_stack[-1].strip()
@@ -693,13 +693,13 @@ class LangServer:
         curr_line, def_char = get_line(def_line, def_char, self.workspace[filepath])
         if curr_line is None:
             return None
-        # Ignore for comment lines
-        if detect_comment_line(curr_line):
-            return None
         #
         is_member = False
         try:
             line_prefix = curr_line[:def_char].lower()
+            # Ignore for comment lines
+            if detect_comment_line(line_prefix):
+                return None
             var_stack = get_var_stack(line_prefix)
             is_member = (len(var_stack) > 1)
             def_name = expand_name(curr_line, def_char)