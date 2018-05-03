hansec/fortran-language-server

commit efa6bfeb1e7392e6523cb8699b74610cb416976c

Add support for unnamed program units, fixes #13
- Resolve item paths from object instead of through global dictionary

fortls/__init__.py
@@ -76,9 +76,12 @@ def main():
             print("\n=========\nParser Output\n=========\n")
             ast_new = process_file(contents_split, True, fixed_flag, True)
             print("\n=========\nObject Tree\n=========\n")
-            for key, obj in ast_new.global_dict.items():
+            for obj in ast_new.get_scopes():
                 print("{0}: {1}".format(obj.get_type(), obj.FQSN))
                 print_children(obj)
+            print("\n=========\nExportable Objects\n=========\n")
+            for key, obj in ast_new.global_dict.items():
+                print("{0}: {1}".format(obj.get_type(), obj.FQSN))
     #
     elif debug_server:
         prb, pwb = os.pipe()

fortls/langserver.py
@@ -44,7 +44,7 @@ def init_file(filepath):
     #
     try:
         fixed_flag = detect_fixed_format(contents_split)
-        ast_new = process_file(contents_split, True, fixed_flag)
+        ast_new = process_file(contents_split, True, filepath, fixed_flag)
     except:
         return None
     else:
@@ -755,12 +755,10 @@ class LangServer:
         var_obj = self.get_definition(path, def_line, def_char)
         # Construct link reference
         if var_obj is not None:
-            var_top_scope = var_obj.FQSN.split("::")[0]
-            if var_top_scope in self.obj_tree:
-                var_path = self.obj_tree[var_top_scope][1]
+            if var_obj.file.path is not None:
                 sline = var_obj.sline-1
                 return {
-                    "uri": path_to_uri(var_path),
+                    "uri": path_to_uri(var_obj.file.path),
                     "range": {
                         "start": {"line": sline, "character": 0},
                         "end": {"line": sline, "character": 1}
@@ -880,7 +878,7 @@ class LangServer:
         # Update workspace from file contents and path
         try:
             fixed_flag = detect_fixed_format(contents_split)
-            ast_new = process_file(contents_split, True, fixed_flag)
+            ast_new = process_file(contents_split, True, filepath, fixed_flag)
         except:
             self.conn.send_notification("window/showMessage", {
                 "type": 1,

fortls/objects.py
@@ -139,10 +139,11 @@ def find_word_in_line(line, word):
 
 
 class fortran_scope:
-    def __init__(self, line_number, name, enc_scope=None):
-        self.base_setup(line_number, name, enc_scope)
+    def __init__(self, file_obj, line_number, name, enc_scope=None):
+        self.base_setup(file_obj, line_number, name, enc_scope)
 
-    def base_setup(self, sline, name, enc_scope=None):
+    def base_setup(self, file_obj, sline, name, enc_scope=None):
+        self.file = file_obj
         self.sline = sline
         self.eline = None
         self.name = name
@@ -273,8 +274,8 @@ class fortran_program(fortran_module):
 
 
 class fortran_subroutine(fortran_scope):
-    def __init__(self, line_number, name, enc_scope=None, args=None):
-        self.base_setup(line_number, name, enc_scope)
+    def __init__(self, file_obj, line_number, name, enc_scope=None, args=None):
+        self.base_setup(file_obj, line_number, name, enc_scope)
         self.args = args
         self.arg_objs = []
 
@@ -330,9 +331,9 @@ class fortran_subroutine(fortran_scope):
 
 
 class fortran_function(fortran_subroutine):
-    def __init__(self, line_number, name, enc_scope=None, args=None,
+    def __init__(self, file_obj, line_number, name, enc_scope=None, args=None,
                  return_type=None, result_var=None):
-        self.base_setup(line_number, name, enc_scope)
+        self.base_setup(file_obj, line_number, name, enc_scope)
         self.args = args
         self.arg_objs = []
         self.result_var = result_var
@@ -359,8 +360,8 @@ class fortran_function(fortran_subroutine):
 
 
 class fortran_type(fortran_scope):
-    def __init__(self, line_number, name, modifiers, enc_scope=None):
-        self.base_setup(line_number, name, enc_scope)
+    def __init__(self, file_obj, line_number, name, modifiers, enc_scope=None):
+        self.base_setup(file_obj, line_number, name, enc_scope)
         #
         self.in_children = []
         self.modifiers = modifiers
@@ -403,8 +404,8 @@ class fortran_type(fortran_scope):
 
 
 class fortran_int(fortran_scope):
-    def __init__(self, line_number, name, enc_scope=None):
-        self.base_setup(line_number, name, enc_scope)
+    def __init__(self, file_obj, line_number, name, enc_scope=None):
+        self.base_setup(file_obj, line_number, name, enc_scope)
         self.mems = []
 
     def get_type(self):
@@ -424,8 +425,9 @@ class fortran_int(fortran_scope):
 
 
 class fortran_obj:
-    def __init__(self, line_number, name, var_desc, modifiers,
+    def __init__(self, file_obj, line_number, name, var_desc, modifiers,
                  enc_scope=None, link_obj=None):
+        self.file = file_obj
         self.sline = line_number
         self.name = name
         self.desc = var_desc
@@ -538,7 +540,8 @@ class fortran_meth(fortran_obj):
 
 
 class fortran_file:
-    def __init__(self):
+    def __init__(self, path=None):
+        self.path = path
         self.global_dict = {}
         self.scope_list = []
         self.variable_list = []
@@ -547,6 +550,7 @@ class fortran_file:
         self.scope_stack = []
         self.end_stack = []
         self.pp_if = []
+        self.none_scope = None
         self.current_scope = None
         self.END_REGEX = None
         self.enc_scope_name = None
@@ -556,13 +560,14 @@ class fortran_file:
             return None
         return self.current_scope.FQSN
 
-    def add_scope(self, new_scope, END_SCOPE_REGEX, hidden=False):
+    def add_scope(self, new_scope, END_SCOPE_REGEX, hidden=False, exportable=True):
         if hidden:
             self.variable_list.append(new_scope)
         else:
             self.scope_list.append(new_scope)
         if self.current_scope is None:
-            self.global_dict[new_scope.FQSN] = new_scope
+            if exportable:
+                self.global_dict[new_scope.FQSN] = new_scope
         else:
             self.current_scope.add_child(new_scope)
             new_scope.add_parent(self.current_scope)
@@ -586,6 +591,12 @@ class fortran_file:
         self.enc_scope_name = self.get_enc_scope_name()
 
     def add_variable(self, new_var):
+        if self.current_scope is None:
+            if self.none_scope is not None:
+                raise ValueError
+            self.none_scope = fortran_program(self, 1, "main")
+            self.add_scope(self.none_scope, re.compile(r'[ \t]*END[ \t]*PROGRAM', re.I), exportable=False)
+            new_var.FQSN = "main::" + new_var.name.lower()
         self.current_scope.add_child(new_var)
         new_var.add_parent(self.current_scope)
         self.variable_list.append(new_var)

fortls/parse_fortran.py
@@ -324,7 +324,7 @@ def_tests = [read_var_def, read_sub_def, read_fun_def, read_type_def,
              read_use_stmt, read_int_def, read_mod_def, read_prog_def]
 
 
-def process_file(file_str, close_open_scopes, fixed_format=False, debug=False):
+def process_file(file_str, close_open_scopes, path=None, fixed_format=False, debug=False):
     #
     if fixed_format:
         COMMENT_LINE_MATCH = FIXED_COMMENT_LINE_MATCH
@@ -333,7 +333,7 @@ def process_file(file_str, close_open_scopes, fixed_format=False, debug=False):
         COMMENT_LINE_MATCH = FREE_COMMENT_LINE_MATCH
         CONT_REGEX = FREE_CONT_REGEX
     #
-    file_obj = fortran_file()
+    file_obj = fortran_file(path)
     line_number = 0
     next_line_num = 1
     int_counter = 0
@@ -459,10 +459,10 @@ def process_file(file_str, close_open_scopes, fixed_format=False, debug=False):
                     name_stripped = name_stripped.split('(')[0].strip()
                     modifiers = parse_keywords(obj[1])
                     if obj[0][:3] == 'PRO':
-                        new_var = fortran_meth(line_number, name_stripped, obj[0],
+                        new_var = fortran_meth(file_obj, line_number, name_stripped, obj[0],
                                                modifiers, file_obj.enc_scope_name, link_name)
                     else:
-                        new_var = fortran_obj(line_number, name_stripped, obj[0],
+                        new_var = fortran_obj(file_obj, line_number, name_stripped, obj[0],
                                               modifiers, file_obj.enc_scope_name, link_name)
                     if var_dim > 0:
                         new_var.set_dim(var_dim)
@@ -470,33 +470,33 @@ def process_file(file_str, close_open_scopes, fixed_format=False, debug=False):
                 if(debug):
                     print('{1} !!! VARIABLE statement({0})'.format(line_number, line.strip()))
             elif obj_type == 'mod':
-                new_mod = fortran_module(line_number, obj, file_obj.enc_scope_name)
+                new_mod = fortran_module(file_obj, line_number, obj, file_obj.enc_scope_name)
                 file_obj.add_scope(new_mod, END_MOD_REGEX)
                 if(debug):
                     print('{1} !!! MODULE statement({0})'.format(line_number, line.strip()))
             elif obj_type == 'prog':
-                new_prog = fortran_program(line_number, obj, file_obj.enc_scope_name)
+                new_prog = fortran_program(file_obj, line_number, obj, file_obj.enc_scope_name)
                 file_obj.add_scope(new_prog, END_PROG_REGEX)
                 if(debug):
                     print('{1} !!! PROGRAM statement({0})'.format(line_number, line.strip()))
             elif obj_type == 'sub':
-                new_sub = fortran_subroutine(line_number, obj[0], file_obj.enc_scope_name, obj[1])
+                new_sub = fortran_subroutine(file_obj, line_number, obj[0], file_obj.enc_scope_name, obj[1])
                 file_obj.add_scope(new_sub, END_SUB_REGEX)
                 if(debug):
                     print('{1} !!! SUBROUTINE statement({0})'.format(line_number, line.strip()))
             elif obj_type == 'fun':
-                new_fun = fortran_function(line_number, obj[0], file_obj.enc_scope_name,
+                new_fun = fortran_function(file_obj, line_number, obj[0], file_obj.enc_scope_name,
                                            obj[1], return_type=obj[2][0], result_var=obj[2][1])
                 file_obj.add_scope(new_fun, END_FUN_REGEX)
                 if obj[2][0] is not None:
-                    new_obj = fortran_obj(line_number, obj[0], obj[2][0][0], obj[2][0][1],
+                    new_obj = fortran_obj(file_obj, line_number, obj[0], obj[2][0][0], obj[2][0][1],
                                           file_obj.enc_scope_name, None)
                     file_obj.add_variable(new_obj)
                 if(debug):
                     print('{1} !!! FUNCTION statement({0})'.format(line_number, line.strip()))
             elif obj_type == 'typ':
                 modifiers = parse_keywords(obj[2])
-                new_type = fortran_type(line_number, obj[0], modifiers, file_obj.enc_scope_name)
+                new_type = fortran_type(file_obj, line_number, obj[0], modifiers, file_obj.enc_scope_name)
                 if obj[1] is not None:
                     new_type.set_inherit(obj[1])
                 file_obj.add_scope(new_type, END_TYPED_REGEX)
@@ -506,7 +506,7 @@ def process_file(file_str, close_open_scopes, fixed_format=False, debug=False):
                 if obj is None:
                     int_counter += 1
                     obj = 'GEN_INT{0}'.format(int_counter)
-                new_int = fortran_int(line_number, obj, file_obj.enc_scope_name)
+                new_int = fortran_int(file_obj, line_number, obj, file_obj.enc_scope_name)
                 file_obj.add_scope(new_int, END_INT_REGEX, True)
                 if(debug):
                     print('{1} !!! INTERFACE statement({0})'.format(line_number, line.strip()))

commit e87a57a9a1d71d18ded73c0c42fe656d54e9fb56

Fix bug where debug output is not reported from parser, introduced in efa6bfe

fortls/__init__.py
@@ -74,7 +74,7 @@ def main():
             fixed_flag = detect_fixed_format(contents_split)
             print('  Detected format: {0}'.format("fixed" if fixed_flag else "free"))
             print("\n=========\nParser Output\n=========\n")
-            ast_new = process_file(contents_split, True, fixed_flag, True)
+            ast_new = process_file(contents_split, True, fixed_format=fixed_flag, debug=True)
             print("\n=========\nObject Tree\n=========\n")
             for obj in ast_new.get_scopes():
                 print("{0}: {1}".format(obj.get_type(), obj.FQSN))
@@ -114,7 +114,7 @@ def main():
                 print("    {0}".format(mod_dir))
         #
         if args.debug_symbols:
-            print('\nTesting "textDocument/definition" request:')
+            print('\nTesting "textDocument/documentSymbol" request:')
             if args.debug_filepath is None:
                 print("  ERROR: 'debug_filepath' not specified for document symbol test")
                 sys.exit(-1)
@@ -134,13 +134,13 @@ def main():
                 }
             })
             for symbol in symbol_results:
-                eline = symbol["location"]["range"]["start"]["line"]
+                sline = symbol["location"]["range"]["start"]["line"]
                 if "containerName" in symbol:
                     parent = symbol["containerName"]
                 else:
                     parent = "null"
-                print('  line {2:5d}  symbol -> {1:3d}:{0:30} parent = {3}'.format(symbol["name"],
-                      symbol["kind"], eline, parent))
+                print('  line {2:5d}  symbol -> {1:3d}:{0:30} parent = {4}'.format(symbol["name"],
+                      symbol["kind"], sline, parent))
         tmpout.close()
         tmpin.close()
     #