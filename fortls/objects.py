hansec/fortran-language-server

commit 8f6d29e3d218cae92d057a06a90c0dfb3569547e

Fix parser error with definitions requiring enclosing scopes in #include files and unnamed programs, fixes #17 - Create temporary enclosing scope for type definitions, use statements and interfaces

fortls/objects.py
@@ -666,19 +666,32 @@ class fortran_file:
         self.END_REGEX = None
         self.enc_scope_name = None
 
+    def create_none_scope(self):
+        if self.none_scope is not None:
+            raise ValueError
+        self.none_scope = fortran_program(self, 1, "main")
+        self.add_scope(self.none_scope, re.compile(r'[ \t]*END[ \t]*PROGRAM', re.I), exportable=False)
+
     def get_enc_scope_name(self):
         if self.current_scope is None:
             return None
         return self.current_scope.FQSN
 
-    def add_scope(self, new_scope, END_SCOPE_REGEX, hidden=False, exportable=True):
+    def add_scope(self, new_scope, END_SCOPE_REGEX, hidden=False, exportable=True, req_container=False):
         if hidden:
             self.variable_list.append(new_scope)
         else:
             self.scope_list.append(new_scope)
         if self.current_scope is None:
-            if exportable:
-                self.global_dict[new_scope.FQSN] = new_scope
+            if req_container:
+                self.create_none_scope()
+                new_scope.FQSN = self.none_scope.FQSN + "::" + new_scope.name.lower()
+                self.current_scope.add_child(new_scope)
+                new_scope.add_parent(self.current_scope)
+                self.scope_stack.append(self.current_scope)
+            else:
+                if exportable:
+                    self.global_dict[new_scope.FQSN] = new_scope
         else:
             self.current_scope.add_child(new_scope)
             new_scope.add_parent(self.current_scope)
@@ -703,11 +716,8 @@ class fortran_file:
 
     def add_variable(self, new_var):
         if self.current_scope is None:
-            if self.none_scope is not None:
-                raise ValueError
-            self.none_scope = fortran_program(self, 1, "main")
-            self.add_scope(self.none_scope, re.compile(r'[ \t]*END[ \t]*PROGRAM', re.I), exportable=False)
-            new_var.FQSN = "main::" + new_var.name.lower()
+            self.create_none_scope()
+            new_var.FQSN = self.none_scope.FQSN + "::" + new_var.name.lower()
         self.current_scope.add_child(new_var)
         new_var.add_parent(self.current_scope)
         self.variable_list.append(new_var)
@@ -722,6 +732,8 @@ class fortran_file:
         self.public_list.append(self.enc_scope_name+'::'+name)
 
     def add_use(self, mod_words, line_number):
+        if self.current_scope is None:
+            self.create_none_scope()
         if len(mod_words) > 0:
             n = len(mod_words)
             if n > 2:

fortls/parse_fortran.py
@@ -537,7 +537,7 @@ def process_file(file_str, close_open_scopes, path=None, fixed_format=False, deb
                 new_type = fortran_type(file_obj, line_number, obj[0], modifiers, file_obj.enc_scope_name)
                 if obj[1] is not None:
                     new_type.set_inherit(obj[1])
-                file_obj.add_scope(new_type, END_TYPED_REGEX)
+                file_obj.add_scope(new_type, END_TYPED_REGEX, req_container=True)
                 if(debug):
                     print('{1} !!! TYPE statement({0})'.format(line_number, line.strip()))
             elif obj_type == 'int':
@@ -547,7 +547,7 @@ def process_file(file_str, close_open_scopes, path=None, fixed_format=False, deb
                     obj = 'GEN_INT{0}'.format(int_counter)
                     hidden = True
                 new_int = fortran_int(file_obj, line_number, obj, file_obj.enc_scope_name)
-                file_obj.add_scope(new_int, END_INT_REGEX, hidden)
+                file_obj.add_scope(new_int, END_INT_REGEX, hidden=hidden, req_container=True)
                 if(debug):
                     print('{1} !!! INTERFACE statement({0})'.format(line_number, line.strip()))
             elif obj_type == 'int_pro':
@@ -560,7 +560,7 @@ def process_file(file_str, close_open_scopes, path=None, fixed_format=False, deb
                 if(debug):
                     print('{1} !!! INTERFACE-PRO statement({0})'.format(line_number, line.strip()))
             elif obj_type == 'use':
-                file_obj.current_scope.add_use(obj[0], line_number, obj[1])
+                file_obj.add_use(obj, line_number)
                 if(debug):
                     print('{1} !!! USE statement({0})'.format(line_number, line.strip()))
         # Look for visiblity statement

commit 355376f87d832e9cdcf61d377d9fc9664ed2d1bb

Fix parsing error with USE statements introduced in 8f6d29e

fortls/objects.py
@@ -731,16 +731,10 @@ class fortran_file:
     def add_public(self, name):
         self.public_list.append(self.enc_scope_name+'::'+name)
 
-    def add_use(self, mod_words, line_number):
+    def add_use(self, mod_word, line_number, only_list):
         if self.current_scope is None:
             self.create_none_scope()
-        if len(mod_words) > 0:
-            n = len(mod_words)
-            if n > 2:
-                use_list = mod_words[2:]
-                self.current_scope.add_use(mod_words[0], line_number, use_list)
-            else:
-                self.current_scope.add_use(mod_words[0], line_number)
+        self.current_scope.add_use(mod_word, line_number, only_list)
 
     def start_ppif(self, line_number):
         self.pp_if.append([line_number-1, -1])

fortls/parse_fortran.py
@@ -560,7 +560,7 @@ def process_file(file_str, close_open_scopes, path=None, fixed_format=False, deb
                 if(debug):
                     print('{1} !!! INTERFACE-PRO statement({0})'.format(line_number, line.strip()))
             elif obj_type == 'use':
-                file_obj.add_use(obj, line_number)
+                file_obj.add_use(obj[0], line_number, obj[1])
                 if(debug):
                     print('{1} !!! USE statement({0})'.format(line_number, line.strip()))
         # Look for visiblity statement