hansec/fortran-language-server

commit 11fe87b1189c5bc4e75ebf6840158f341b1bb58c

Handle type bound procedures with explicit pass statements (#33)
- Fix linking of type bound procedures with same name of bound procedure and subroutine/function definition

fortls/objects.py
@@ -7,6 +7,7 @@ CLASS_VAR_REGEX = re.compile(r'(TYPE|CLASS)[ ]*\(', re.I)
 
 def map_keywords(keywords):
     modifiers = []
+    pass_name = None
     for key in keywords:
         key_lower = key.lower()
         if key_lower == 'pointer':
@@ -40,8 +41,13 @@ def map_keywords(keywords):
         elif key_lower.startswith('dimension'):
             ndims = key_lower.count(':')
             modifiers.append(20+ndims)
+        elif key_lower.startswith('pass'):
+            i1 = key_lower.find('(')
+            i2 = key_lower.find(')')
+            if i1 > -1 and i2 > i1:
+                pass_name = key_lower[i1+1:i2]
     modifiers.sort()
-    return modifiers
+    return modifiers, pass_name
 
 
 def get_keywords(modifiers):
@@ -248,7 +254,7 @@ class fortran_scope:
     def get_desc(self):
         return 'unknown'
 
-    def get_snippet(self, name_replace=None, drop_arg=False):
+    def get_snippet(self, name_replace=None, drop_arg=-1):
         if name_replace is not None:
             return name_replace, None
         return self.name, None
@@ -454,13 +460,10 @@ class fortran_subroutine(fortran_scope):
     def get_type(self):
         return 2
 
-    def get_snippet(self, name_replace=None, drop_arg=False):
+    def get_snippet(self, name_replace=None, drop_arg=-1):
         arg_list = self.args_snip.split(",")
-        if drop_arg:
-            if len(arg_list) > 1:
-                arg_list = arg_list[1:]
-            else:
-                arg_list = []
+        if (drop_arg >= 0) and (drop_arg < len(arg_list)):
+            del arg_list[drop_arg]
         arg_snip = None
         if len(arg_list) > 0:
             place_holders = []
@@ -763,7 +766,7 @@ class fortran_obj:
                 return
         self.modifiers.append(ndim+20)
 
-    def get_snippet(self, name_replace=None, drop_arg=False):
+    def get_snippet(self, name_replace=None, drop_arg=-1):
         name = self.name
         if name_replace is not None:
             name = name_replace
@@ -806,10 +809,11 @@ class fortran_obj:
 
 class fortran_meth(fortran_obj):
     def __init__(self, file_obj, line_number, name, var_desc, modifiers,
-                 enc_scope=None, link_obj=None):
+                 enc_scope=None, link_obj=None, pass_name=None):
         self.base_setup(file_obj, line_number, name, var_desc, modifiers,
                         enc_scope, link_obj)
-        self.drop_arg = False
+        self.drop_arg = -1
+        self.pass_name = pass_name
         if link_obj is None:
             open_paren = var_desc.find('(')
             close_paren = var_desc.find(')')
@@ -819,9 +823,9 @@ class fortran_meth(fortran_obj):
     def set_parent(self, parent_obj):
         self.parent = parent_obj
         if (self.parent.get_type() == 4) and (self.modifiers.count(6) == 0):
-            self.drop_arg = True
+            self.drop_arg = 0
 
-    def get_snippet(self, name_replace=None, drop_arg=False):
+    def get_snippet(self, name_replace=None, drop_arg=-1):
         name = self.name
         if name_replace is not None:
             name = name_replace
@@ -847,18 +851,14 @@ class fortran_meth(fortran_obj):
                 link_name_len = len(self.link_obj.name)
                 call_sig = call_sig[:paren_start-link_name_len] + self.name + call_sig[paren_start:]
                 paren_start += len(self.name) - link_name_len
-                if self.drop_arg:
+                hover_split = hover_split[1:]
+                if (self.drop_arg >= 0) and (self.drop_arg < len(hover_split)):
                     paren_end = call_sig.rfind(')')
                     args = call_sig[paren_start+1:paren_end].split(',')
-                    if len(args) > 1:
-                        args = args[1:]
-                    else:
-                        args = []
+                    del args[self.drop_arg]
+                    del hover_split[self.drop_arg]
                     call_sig = call_sig[:paren_start] + '(' + (','.join(args)).strip() + ')'
-                    arg_list = hover_split[2:]
-                else:
-                    arg_list = hover_split[1:]
-                hover_str = '\n'.join([call_sig] + arg_list)
+                hover_str = '\n'.join([call_sig] + hover_split)
             return hover_str, True
         else:
             doc_str = self.desc
@@ -872,7 +872,7 @@ class fortran_meth(fortran_obj):
         var_obj = self.link_obj
         arg_list = var_obj.args.split(",")
         for i, arg_obj in enumerate(var_obj.arg_objs):
-            if self.drop_arg and (i == 0):
+            if self.drop_arg == i:
                 continue
             if arg_obj is None:
                 arg_sigs.append({"label": arg_list[i]})
@@ -887,6 +887,23 @@ class fortran_meth(fortran_obj):
                 })
         call_sig, _ = self.get_snippet()
         return call_sig, None, arg_sigs
+
+    def resolve_link(self, obj_tree):
+        if self.link_name is None:
+            return
+        if self.parent is not None:
+            if self.parent.get_type() == 4:
+                link_obj, _ = find_in_scope(self.parent.parent, self.link_name, obj_tree)
+            else:
+                link_obj, _ = find_in_scope(self.parent, self.link_name, obj_tree)
+            if link_obj is not None:
+                self.link_obj = link_obj
+                if self.pass_name is not None:
+                    self.pass_name = self.pass_name.lower()
+                    for i, arg in enumerate(link_obj.args_snip.split(',')):
+                        if arg.lower() == self.pass_name:
+                            self.drop_arg = i
+                            break
 
     def is_callable(self):
         return True

fortls/parse_fortran.py
@@ -32,7 +32,8 @@ KIND_SPEC_REGEX = re.compile(r'([ ]*\([a-z0-9_ =*]*\)|\*[0-9]*)', re.I)
 KEYWORD_LIST_REGEX = re.compile(r'[ ]*,[ ]*(PUBLIC|PRIVATE|ALLOCATABLE|'
                                 r'POINTER|TARGET|DIMENSION\([a-z0-9_:, ]*\)|'
                                 r'OPTIONAL|INTENT\([inout]*\)|DEFERRED|NOPASS|'
-                                r'SAVE|PARAMETER|CONTIGUOUS)', re.I)
+                                r'PASS\([a-z0-9_]*\)|SAVE|PARAMETER|'
+                                r'CONTIGUOUS)', re.I)
 TATTR_LIST_REGEX = re.compile(r'[ ]*,[ ]*(PUBLIC|PRIVATE|ABSTRACT|EXTENDS\([a-z0-9_]*\))', re.I)
 VIS_REGEX = re.compile(r'(PUBLIC|PRIVATE)', re.I)
 WORD_REGEX = re.compile(r'[a-z_][a-z0-9_]*', re.I)
@@ -521,10 +522,11 @@ def process_file(file_str, close_open_scopes, path=None, fixed_format=False, deb
                     if name_stripped.find('(') > -1:
                         var_dim = get_var_dims(name_stripped)
                     name_stripped = name_stripped.split('(')[0].strip()
-                    modifiers = map_keywords(obj[1])
+                    modifiers, pass_name = map_keywords(obj[1])
                     if obj[0][:3] == 'PRO':
                         new_var = fortran_meth(file_obj, line_number, name_stripped, obj[0],
-                                               modifiers, file_obj.enc_scope_name, link_name)
+                                               modifiers, file_obj.enc_scope_name, link_name,
+                                               pass_name=pass_name)
                     else:
                         new_var = fortran_obj(file_obj, line_number, name_stripped, obj[0],
                                               modifiers, file_obj.enc_scope_name, link_name)
@@ -573,7 +575,7 @@ def process_file(file_str, close_open_scopes, path=None, fixed_format=False, deb
                 if(debug):
                     print('{1} !!! BLOCK statement({0})'.format(line_number, line.strip()))
             elif obj_type == 'typ':
-                modifiers = map_keywords(obj[2])
+                modifiers, _ = map_keywords(obj[2])
                 new_type = fortran_type(file_obj, line_number, obj[0], modifiers, file_obj.enc_scope_name)
                 if obj[1] is not None:
                     new_type.set_inherit(obj[1])

test/test_server.py
@@ -1,10 +1,10 @@
 import subprocess
+import sys
+import os
 try:
     import StringIO.StringIO as StringIO
 except:
     from io import StringIO
-import sys
-import os
 root_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
 sys.path.insert(0, root_dir)
 from fortls.jsonrpc import write_rpc_request, write_rpc_notification, read_rpc_messages
@@ -81,7 +81,7 @@ def test_change():
             "text": "",
             "range": {
                 "start": {"line": 6, "character": 0},
-                "end": {"line": 29, "character": 0}
+                "end": {"line": 31, "character": 0}
             }
         },
         {
@@ -99,33 +99,36 @@ def test_change():
     #
     assert errcode == 0
     assert len(results) == 2
-    assert len(results[1]) == 4
+    assert len(results[1]) == 5
 
 
 def test_symbols():
     def check_return(result_array):
         # Expected objects
-        objs = [
-            ["test_free", 2, 0, 69],
+        objs = (
+            ["test_free", 2, 0, 77],
             ["scale_type", 5, 4, 6],
             ["val", 13, 5, 5],
-            ["vector", 5, 8, 14],
+            ["vector", 5, 8, 16],
             ["n", 13, 9, 9],
             ["v", 13, 10, 10],
-            ["create", 6, 12, 12],
-            ["norm", 12, 13, 13],
-            ["scaled_vector", 5, 16, 21],
-            ["scale", 13, 17, 17],
-            ["set_scale", 6, 19, 19],
-            ["norm", 12, 20, 20],
-            ["fort_wrap", 11, 24, 27],
-            ["vector_create", 6, 33, 39],
-            ["vector_norm", 12, 41, 45],
-            ["scaled_vector_set", 6, 47, 51],
-            ["scaled_vector_norm", 12, 53, 57],
-            ["unscaled_norm", 12, 59, 63],
-            ["test_sig_Sub", 6, 65, 68]
-        ]
+            ["bound_nopass", 6, 11, 11],
+            ["create", 6, 13, 13],
+            ["norm", 12, 14, 14],
+            ["bound_pass", 6, 15, 15],
+            ["scaled_vector", 5, 18, 23],
+            ["scale", 13, 19, 19],
+            ["set_scale", 6, 21, 21],
+            ["norm", 12, 22, 22],
+            ["fort_wrap", 11, 26, 29],
+            ["vector_create", 6, 35, 41],
+            ["vector_norm", 12, 43, 47],
+            ["scaled_vector_set", 6, 49, 53],
+            ["scaled_vector_norm", 12, 55, 59],
+            ["unscaled_norm", 12, 61, 65],
+            ["test_sig_Sub", 6, 67, 70],
+            ["bound_pass", 6, 72, 76]
+        )
         assert len(result_array) == len(objs)
         for i, obj in enumerate(objs):
             assert result_array[i]["name"] == obj[0]
@@ -149,103 +152,68 @@ def test_comp():
         assert len(result_array["items"]) == checks[0]
         if checks[0] > 0:
             assert result_array["items"][0]["label"] == checks[1]
+
+    def comp_request(file_path, line, char):
+        return write_rpc_request(1, "textDocument/completion", {
+            "textDocument": {"uri": file_path},
+            "position": {"line": line, "character": char}
+        })
     #
     string = write_rpc_request(1, "initialize", {"rootPath": test_dir})
     file_path = os.path.join(test_dir, "test_prog.f08")
-    string += write_rpc_request(2, "textDocument/completion", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 12, "character": 6}
-    })
-    string += write_rpc_request(3, "textDocument/completion", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 13, "character": 6}
-    })
-    string += write_rpc_request(4, "textDocument/completion", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 18, "character": 7}
-    })
-    string += write_rpc_request(5, "textDocument/completion", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 19, "character": 20}
-    })
-    string += write_rpc_request(6, "textDocument/completion", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 19, "character": 42}
-    })
-    string += write_rpc_request(7, "textDocument/completion", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 21, "character": 26}
-    })
+    string += comp_request(file_path, 12, 6)
+    string += comp_request(file_path, 13, 6)
+    string += comp_request(file_path, 17, 24)
+    string += comp_request(file_path, 18, 23)
+    string += comp_request(file_path, 20, 7)
+    string += comp_request(file_path, 21, 20)
+    string += comp_request(file_path, 21, 42)
+    string += comp_request(file_path, 23, 26)
     file_path = os.path.join(test_dir, "subdir", "test_submod.F90")
-    string += write_rpc_request(8, "textDocument/completion", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 16, "character": 12}
-    })
-    string += write_rpc_request(9, "textDocument/completion", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 17, "character": 8}
-    })
-    string += write_rpc_request(10, "textDocument/completion", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 17, "character": 23}
-    })
+    string += comp_request(file_path, 16, 12)
+    string += comp_request(file_path, 17, 8)
+    string += comp_request(file_path, 17, 23)
     file_path = os.path.join(test_dir, "test_inc.f90")
-    string += write_rpc_request(11, "textDocument/completion", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 10, "character": 2}
-    })
+    string += comp_request(file_path, 10, 2)
     file_path = os.path.join(test_dir, "subdir", "test_inc2.f90")
-    string += write_rpc_request(12, "textDocument/completion", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 3, "character": 2}
-    })
+    string += comp_request(file_path, 3, 2)
     file_path = os.path.join(test_dir, "subdir", "test_abstract.f90")
-    string += write_rpc_request(13, "textDocument/completion", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 7, "character": 12}
-    })
+    string += comp_request(file_path, 7, 12)
     file_path = os.path.join(test_dir, "subdir", "test_free.f90")
-    string += write_rpc_request(14, "textDocument/completion", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 10, "character": 22}
-    })
-    string += write_rpc_request(15, "textDocument/completion", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 26, "character": 14}
-    })
+    string += comp_request(file_path, 10, 22)
+    string += comp_request(file_path, 28, 14)
     file_path = os.path.join(test_dir, "test_block.f08")
-    string += write_rpc_request(16, "textDocument/completion", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 2, "character": 2}
-    })
-    string += write_rpc_request(17, "textDocument/completion", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 5, "character": 4}
-    })
-    string += write_rpc_request(18, "textDocument/completion", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 8, "character": 6}
-    })
+    string += comp_request(file_path, 2, 2)
+    string += comp_request(file_path, 5, 4)
+    string += comp_request(file_path, 8, 6)
     errcode, results = run_request(string)
-    #
     assert errcode == 0
-    check_return(results[1], [1, "myfun(n, xval)"])
-    check_return(results[2], [4, "glob_sub(n, xval, yval)"])
-    check_return(results[3], [1, "stretch_vector"])
-    check_return(results[4], [4, "scale"])
-    check_return(results[5], [2, "n"])
-    check_return(results[6], [1, "val"])
-    check_return(results[7], [1, "point"])
-    check_return(results[8], [1, "distance"])
-    check_return(results[9], [2, "x"])
-    check_return(results[10], [2, "val1"])
-    check_return(results[11], [2, "val1"])
-    check_return(results[12], [1, "abs_interface"])
-    check_return(results[13], [1, "DIMENSION(:)"])
-    check_return(results[14], [3, "INTENT(IN)"])
-    check_return(results[15], [3, "res0"])
-    check_return(results[16], [4, "res0"])
-    check_return(results[17], [5, "res0"])
+    #
+    exp_results = (
+        [1, "myfun(n, xval)"],
+        [4, "glob_sub(n, xval, yval)"],
+        [1, "bound_nopass(a, b)"],
+        [1, "bound_pass(arg1)"],
+        [1, "stretch_vector"],
+        [6, "scale"],
+        [2, "n"],
+        [1, "val"],
+        [1, "point"],
+        [1, "distance"],
+        [2, "x"],
+        [2, "val1"],
+        [2, "val1"],
+        [1, "abs_interface"],
+        [1, "DIMENSION(:)"],
+        [3, "INTENT(IN)"],
+        [3, "res0"],
+        [4, "res0"],
+        [5, "res0"]
+    )
+    assert len(exp_results)+1 == len(results)
+    for i in range(len(exp_results)):
+        check_return(results[i+1], exp_results[i])
+
 
 def test_sig():
     def check_return(results, checks):
@@ -253,106 +221,88 @@ def test_sig():
         signatures = results.get('signatures')
         assert signatures[0].get('label') == checks[2]
         assert len(signatures[0].get('parameters')) == checks[1]
+
+    def sig_request(file_path, line, char):
+        return write_rpc_request(1, "textDocument/signatureHelp", {
+            "textDocument": {"uri": file_path},
+            "position": {"line": line, "character": char}
+        })
     #
     string = write_rpc_request(1, "initialize", {"rootPath": test_dir})
     file_path = os.path.join(test_dir, "test_prog.f08")
-    string += write_rpc_request(2, "textDocument/signatureHelp", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 23, "character": 18}
-    })
-    string += write_rpc_request(2, "textDocument/signatureHelp", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 23, "character": 20}
-    })
-    string += write_rpc_request(2, "textDocument/signatureHelp", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 23, "character": 22}
-    })
-    string += write_rpc_request(2, "textDocument/signatureHelp", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 23, "character": 27}
-    })
-    string += write_rpc_request(2, "textDocument/signatureHelp", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 23, "character": 29}
-    })
+    string += sig_request(file_path, 25, 18)
+    string += sig_request(file_path, 25, 20)
+    string += sig_request(file_path, 25, 22)
+    string += sig_request(file_path, 25, 27)
+    string += sig_request(file_path, 25, 29)
     errcode, results = run_request(string)
-    #
     assert errcode == 0
+    #
     sub_sig = "test_sig_Sub(arg1, arg2, opt1=opt1, opt2=opt2, opt3=opt3)"
-    check_return(results[1], [0, 5, sub_sig])
-    check_return(results[2], [1, 5, sub_sig])
-    check_return(results[3], [2, 5, sub_sig])
-    check_return(results[4], [3, 5, sub_sig])
-    check_return(results[5], [4, 5, sub_sig])
+    exp_results = (
+        [0, 5, sub_sig],
+        [1, 5, sub_sig],
+        [2, 5, sub_sig],
+        [3, 5, sub_sig],
+        [4, 5, sub_sig]
+    )
+    assert len(exp_results)+1 == len(results)
+    for i in range(len(exp_results)):
+        check_return(results[i+1], exp_results[i])
 
 
 def test_def():
     def check_return(result_array, checks):
-        assert result_array["uri"] == checks[2]
+        assert result_array["uri"] == path_to_uri(checks[2])
         assert result_array["range"]["start"]["line"] == checks[0]
         assert result_array["range"]["start"]["line"] == checks[1]
+
+    def def_request(file_path, line, char):
+        return write_rpc_request(1, "textDocument/definition", {
+            "textDocument": {"uri": file_path},
+            "position": {"line": line, "character": char}
+        })
     #
     string = write_rpc_request(1, "initialize", {"rootPath": test_dir})
     file_path = os.path.join(test_dir, "test_prog.f08")
-    string += write_rpc_request(2, "textDocument/definition", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 12, "character": 6}
-    })
-    string += write_rpc_request(3, "textDocument/definition", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 13, "character": 6}
-    })
-    string += write_rpc_request(4, "textDocument/definition", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 18, "character": 7}
-    })
-    string += write_rpc_request(5, "textDocument/definition", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 19, "character": 20}
-    })
-    string += write_rpc_request(6, "textDocument/definition", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 19, "character": 42}
-    })
-    string += write_rpc_request(7, "textDocument/definition", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 21, "character": 26}
-    })
+    string += def_request(file_path, 12, 6)
+    string += def_request(file_path, 13, 6)
+    string += def_request(file_path, 20, 7)
+    string += def_request(file_path, 21, 20)
+    string += def_request(file_path, 21, 42)
+    string += def_request(file_path, 23, 26)
     file_path = os.path.join(test_dir, "subdir", "test_submod.F90")
-    string += write_rpc_request(8, "textDocument/definition", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 16, "character": 12}
-    })
+    string += def_request(file_path, 16, 12)
     file_path = os.path.join(test_dir, "test_inc.f90")
-    string += write_rpc_request(9, "textDocument/definition", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 10, "character": 2}
-    })
+    string += def_request(file_path, 10, 2)
     file_path = os.path.join(test_dir, "subdir", "test_inc2.f90")
-    string += write_rpc_request(10, "textDocument/definition", {
-        "textDocument": {"uri": file_path},
-        "position": {"line": 3, "character": 2}
-    })
+    string += def_request(file_path, 3, 2)
     errcode, results = run_request(string)
-    #
     assert errcode == 0
-    check_return(results[1], [0, 0, path_to_uri(os.path.join(test_dir, "subdir", "test_fixed.f"))])
-    check_return(results[2], [20, 20, path_to_uri(os.path.join(test_dir, "subdir", "test_fixed.f"))])
-    check_return(results[3], [10, 10, path_to_uri(os.path.join(test_dir, "test_prog.f08"))])
-    check_return(results[4], [19, 19, path_to_uri(os.path.join(test_dir, "subdir", "test_free.f90"))])
-    check_return(results[5], [13, 13, path_to_uri(os.path.join(test_dir, "subdir", "test_free.f90"))])
-    check_return(results[6], [5, 5, path_to_uri(os.path.join(test_dir, "subdir", "test_free.f90"))])
-    check_return(results[7], [1, 1, path_to_uri(os.path.join(test_dir, "subdir", "test_submod.F90"))])
-    check_return(results[8], [0, 0, path_to_uri(os.path.join(test_dir, "subdir", "test_inc2.f90"))])
-    check_return(results[9], [4, 4, path_to_uri(os.path.join(test_dir, "test_inc.f90"))])
+    #
+    fixed_path = os.path.join(test_dir, "subdir", "test_fixed.f")
+    free_path = os.path.join(test_dir, "subdir", "test_free.f90")
+    exp_results = (
+        [0, 0, fixed_path],
+        [20, 20, fixed_path],
+        [10, 10, os.path.join(test_dir, "test_prog.f08")],
+        [21, 21, free_path],
+        [14, 14, free_path],
+        [5, 5, free_path],
+        [1, 1, os.path.join(test_dir, "subdir", "test_submod.F90")],
+        [0, 0, os.path.join(test_dir, "subdir", "test_inc2.f90")],
+        [4, 4, os.path.join(test_dir, "test_inc.f90")]
+    )
+    assert len(exp_results)+1 == len(results)
+    for i in range(len(exp_results)):
+        check_return(results[i+1], exp_results[i])
 
 
 def test_refs():
     def check_return(result_array, checks):
         assert len(result_array) == len(checks)
-        for (i,check) in enumerate(checks):
-            assert result_array[i]["uri"] == check[2]
+        for (i, check) in enumerate(checks):
+            assert result_array[i]["uri"] == path_to_uri(check[2])
             assert result_array[i]["range"]["start"]["character"] == check[0]
             assert result_array[i]["range"]["end"]["character"] == check[1]
     #
@@ -363,15 +313,17 @@ def test_refs():
         "position": {"line": 9, "character": 8}
     })
     errcode, results = run_request(string)
-    #
     assert errcode == 0
-    check_return(results[1], [
-        [8,  14, path_to_uri(os.path.join(test_dir, "subdir", "test_free.f90"))],
-        [9,  15, path_to_uri(os.path.join(test_dir, "subdir", "test_free.f90"))],
-        [14, 20, path_to_uri(os.path.join(test_dir, "subdir", "test_free.f90"))],
-        [6,  12, path_to_uri(os.path.join(test_dir, "subdir", "test_free.f90"))],
-        [6,  12, path_to_uri(os.path.join(test_dir, "subdir", "test_free.f90"))],
-        [6,  12, path_to_uri(os.path.join(test_dir, "subdir", "test_free.f90"))],
-        [21, 27, path_to_uri(os.path.join(test_dir, "test_prog.f08"))],
-        [5,  11, path_to_uri(os.path.join(test_dir, "test_prog.f08"))]
-    ])
+    #
+    free_path = os.path.join(test_dir, "subdir", "test_free.f90")
+    check_return(results[1], (
+        [8, 14, free_path],
+        [9, 15, free_path],
+        [14, 20, free_path],
+        [6, 12, free_path],
+        [6, 12, free_path],
+        [6, 12, free_path],
+        [6, 12, free_path],
+        [21, 27, os.path.join(test_dir, "test_prog.f08")],
+        [5, 11, os.path.join(test_dir, "test_prog.f08")]
+    ))

test/test_source/subdir/test_free.f90
@@ -9,9 +9,11 @@ END TYPE scale_type
 TYPE :: vector
   INTEGER(4) :: n
   REAL(8), POINTER, DIMENSION(:) :: v => NULL()
+  PROCEDURE(fort_wrap), NOPASS, POINTER :: bound_nopass => NULL()
 CONTAINS
   PROCEDURE :: create => vector_create
   PROCEDURE :: norm => vector_norm
+  PROCEDURE, PASS(self) :: bound_pass => bound_pass
 END TYPE vector
 !
 TYPE, EXTENDS(vector) :: scaled_vector
@@ -67,4 +69,10 @@ SUBROUTINE test_sig_Sub(arg1,arg2,opt1,opt2,opt3)
 INTEGER, INTENT(in) :: arg1,arg2
 INTEGER, OPTIONAL, INTENT(in) :: opt1,opt2,opt3
 END SUBROUTINE test_sig_Sub
+!
+SUBROUTINE bound_pass(arg1, self)
+INTEGER(4), INTENT(in) :: arg1
+CLASS(vector), INTENT(inout) :: self
+self%n = arg1
+END SUBROUTINE bound_pass
 END MODULE test_free

test/test_source/test_prog.f08
@@ -15,6 +15,8 @@ CALL glob_sub(n,xc,yc)
 !
 CALL loc_vector%create(n)
 x = loc_vector%norm()
+CALL loc_vector%bound_nopass(a,x)
+CALL loc_vector%bound_pass(n)
 !
 CALL stretch_vector%create(n)
 CALL stretch_vector%set_scale(loc_vector%norm(self))

commit 8b034c0f0a5b07385e3b1b846ed4534cea3a382d

Fix crash in completion requests introduced in 11fe87b

fortls/intrinsics.py
@@ -129,7 +129,7 @@ def get_intrinsic_modules():
         if "args" in json_obj:
             args = json_obj["args"]
         if "mods" in json_obj:
-            modifiers = map_keywords(json_obj["mods"])
+            modifiers, _ = map_keywords(json_obj["mods"])
         name = json_obj["name"]
         if lowercase_intrinsics:
             name = name.lower()

fortls/objects.py
@@ -535,7 +535,7 @@ class fortran_function(fortran_subroutine):
         self.result_obj = None
         if return_type is not None:
             self.return_type = return_type[0]
-            self.modifiers = map_keywords(return_type[1])
+            self.modifiers, _ = map_keywords(return_type[1])
         else:
             self.return_type = None
             self.modifiers = []