fd0561de77c836cd3dac1dd1684ed9ef17b009ac
Chris Hansen <hansec@uw.edu>
2018-09-10 10:58:35 -0400

    Only apply encoding to change requests using Python2, fix unicode bug introduced in 4f2a16d

--- fortls/langserver.py
+++ fortls/langserver.py
@@ -276,8 +276,10 @@ def get_line(line, character, file_obj):
 
 def apply_change(contents_split, change):
     """Apply a change to the document."""
-    text = change.get('text', "").encode('utf-8')
+    text = change.get('text', "")
     change_range = change.get('range')
+    if not PY3K:
+        text = text.encode('utf-8')
     if len(text) == 0:
         text_split = [""]
     else:
@@ -315,7 +317,7 @@ def apply_change(contents_split, change):
         if i == start_line:
             for j, change_line in enumerate(text_split):
                 if j == 0:
-                    new_contents.append(line[:start_col]+change_line)
+                    new_contents.append(line[:start_col] + change_line)
                 else:
                     new_contents.append(change_line)
 
@@ -1173,7 +1175,10 @@ class LangServer:
         path = path_from_uri(uri)
         # Update file contents with changes
         if self.sync_type == 1:
-            new_contents = params["contentChanges"][0]["text"].encode('utf-8').splitlines()
+            file_text = params["contentChanges"][0]["text"]
+            if not PY3K:
+                file_text = file_text.encode('utf-8')
+            new_contents = file_text.splitlines()
         else:
             if path in self.workspace:
                 new_contents = self.workspace[path]["contents"]

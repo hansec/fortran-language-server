hansec/fortran-language-server

commit 4f2a16d01263178cc1b6ba2a461613c8f50344b0

Encode text as UTF-8 in change requests, fixes #41

fortls/langserver.py
@@ -276,7 +276,7 @@ def get_line(line, character, file_obj):
 
 def apply_change(contents_split, change):
     """Apply a change to the document."""
-    text = change.get('text', "")
+    text = change.get('text', "").encode('utf-8')
     change_range = change.get('range')
     if len(text) == 0:
         text_split = [""]
@@ -1173,7 +1173,7 @@ class LangServer:
         path = path_from_uri(uri)
         # Update file contents with changes
         if self.sync_type == 1:
-            new_contents = params["contentChanges"][0]["text"].splitlines()
+            new_contents = params["contentChanges"][0]["text"].encode('utf-8').splitlines()
         else:
             if path in self.workspace:
                 new_contents = self.workspace[path]["contents"]

test/test_server.py
@@ -78,6 +78,13 @@ def test_change():
     string += write_rpc_notification("textDocument/didChange", {
         "textDocument": {"uri": file_path},
         "contentChanges": [{
+            "text": " unicode test",
+            "range": {
+                "start": {"line": 3, "character": 3},
+                "end": {"line": 3, "character": 3}
+            }
+        },
+        {
             "text": "",
             "range": {
                 "start": {"line": 6, "character": 0},

test/test_source/subdir/test_free.f90
@@ -1,7 +1,7 @@
 MODULE test_free
 USE, INTRINSIC :: iso_fortran_env, ONLY: error_unit
 IMPLICIT NONE
-!
+! ą
 TYPE :: scale_type
   REAL(8) :: val = 1.d0
 END TYPE scale_type

commit fd0561de77c836cd3dac1dd1684ed9ef17b009ac

Only apply encoding to change requests using Python2, fix unicode bug introduced in 4f2a16d

fortls/langserver.py
@@ -276,8 +276,10 @@ def get_line(line, character, file_obj):
 
 def apply_change(contents_split, change):
     """Apply a change to the document."""
-    text = change.get('text', "").encode('utf-8')
+    text = change.get('text', "")
     change_range = change.get('range')
+    if not PY3K:
+        text = text.encode('utf-8')
     if len(text) == 0:
         text_split = [""]
     else:
@@ -315,7 +317,7 @@ def apply_change(contents_split, change):
         if i == start_line:
             for j, change_line in enumerate(text_split):
                 if j == 0:
-                    new_contents.append(line[:start_col]+change_line)
+                    new_contents.append(line[:start_col] + change_line)
                 else:
                     new_contents.append(change_line)
 
@@ -1173,7 +1175,10 @@ class LangServer:
         path = path_from_uri(uri)
         # Update file contents with changes
         if self.sync_type == 1:
-            new_contents = params["contentChanges"][0]["text"].encode('utf-8').splitlines()
+            file_text = params["contentChanges"][0]["text"]
+            if not PY3K:
+                file_text = file_text.encode('utf-8')
+            new_contents = file_text.splitlines()
         else:
             if path in self.workspace:
                 new_contents = self.workspace[path]["contents"]