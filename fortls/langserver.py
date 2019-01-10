hansec/fortran-language-server

commit a78123959f8c1d643d6d179322a93e2ebbf921c9

Add parsing of DO/IF/WHERE blocks and report scope end errors

fortls/langserver.py
@@ -630,7 +630,7 @@ class LangServer:
                 return type
 
         def set_type_mask(def_value):
-            return [def_value for i in range(10)]
+            return [def_value if i < 8 else True for i in range(12)]
 
         def get_candidates(scope_list, var_prefix, inc_globals=True, public_only=False, abstract_only=False):
             #
@@ -815,8 +815,6 @@ class LangServer:
         type_mask = set_type_mask(False)
         type_mask[1] = True
         type_mask[4] = True
-        type_mask[8] = True
-        type_mask[9] = True
         if line_context == 1:
             # Use statement module part (modules only)
             for key in self.obj_tree:

fortls/objects.py
@@ -299,6 +299,9 @@ class fortran_scope:
     def is_abstract(self):
         return False
 
+    def req_named_end(self):
+        return False
+
     def end(self, line_number):
         self.eline = line_number
 
@@ -664,8 +667,44 @@ class fortran_block(fortran_scope):
     def get_children(self, public_only=False):
         return self.children
 
+    def req_named_end(self):
+        return True
 
-class fortran_select(fortran_scope):
+
+class fortran_do(fortran_block):
+    def __init__(self, file_obj, line_number, name, enc_scope=None):
+        self.base_setup(file_obj, line_number, name, enc_scope)
+
+    def get_type(self):
+        return 10
+
+    def get_desc(self):
+        return 'DO'
+
+
+class fortran_where(fortran_block):
+    def __init__(self, file_obj, line_number, name, enc_scope=None):
+        self.base_setup(file_obj, line_number, name, enc_scope)
+
+    def get_type(self):
+        return 11
+
+    def get_desc(self):
+        return 'WHERE'
+
+
+class fortran_if(fortran_block):
+    def __init__(self, file_obj, line_number, name, enc_scope=None):
+        self.base_setup(file_obj, line_number, name, enc_scope)
+
+    def get_type(self):
+        return 12
+
+    def get_desc(self):
+        return 'IF'
+
+
+class fortran_select(fortran_block):
     def __init__(self, file_obj, line_number, name, select_info, enc_scope=None):
         self.base_setup(file_obj, line_number, name, enc_scope)
         self.type = select_info[0]
@@ -686,10 +725,7 @@ class fortran_select(fortran_scope):
         return 9
 
     def get_desc(self):
-        return r'SELECT'
-
-    def get_children(self, public_only=False):
-        return self.children
+        return 'SELECT'
 
 
 class fortran_int(fortran_scope):
@@ -956,10 +992,11 @@ class fortran_file:
         self.end_stack = []
         self.pp_if = []
         self.include_stmnts = []
+        self.end_errors = []
         self.none_scope = None
         self.inc_scope = None
         self.current_scope = None
-        self.END_REGEX = None
+        self.END_SCOPE_WORD = None
         self.enc_scope_name = None
 
     def create_none_scope(self):
@@ -973,7 +1010,7 @@ class fortran_file:
             return None
         return self.current_scope.FQSN
 
-    def add_scope(self, new_scope, END_SCOPE_REGEX, exportable=True, req_container=False):
+    def add_scope(self, new_scope, END_SCOPE_WORD, exportable=True, req_container=False):
         self.scope_list.append(new_scope)
         if self.current_scope is None:
             if req_container:
@@ -987,10 +1024,10 @@ class fortran_file:
         else:
             self.current_scope.add_child(new_scope)
             self.scope_stack.append(self.current_scope)
-        if self.END_REGEX is not None:
-            self.end_stack.append(self.END_REGEX)
+        if self.END_SCOPE_WORD is not None:
+            self.end_stack.append(self.END_SCOPE_WORD)
         self.current_scope = new_scope
-        self.END_REGEX = END_SCOPE_REGEX
+        self.END_SCOPE_WORD = END_SCOPE_WORD
         self.enc_scope_name = self.get_enc_scope_name()
 
     def end_scope(self, line_number):
@@ -1000,9 +1037,9 @@ class fortran_file:
         else:
             self.current_scope = None
         if len(self.end_stack) > 0:
-            self.END_REGEX = self.end_stack.pop()
+            self.END_SCOPE_WORD = self.end_stack.pop()
         else:
-            self.END_REGEX = None
+            self.END_SCOPE_WORD = None
         self.enc_scope_name = self.get_enc_scope_name()
 
     def add_variable(self, new_var):
@@ -1147,6 +1184,15 @@ class fortran_file:
         tmp_list = self.scope_list
         if self.none_scope is not None:
             tmp_list += [self.none_scope]
+        for error in self.end_errors:
+            errors.append({
+                "range": {
+                    "start": {"line": error[1]-1, "character": 0},
+                    "end": {"line": error[1]-1, "character": 0}
+                },
+                "message": 'Unexpected end of scope at line {0}'.format(error[0]),
+                "severity": 1
+            })
         for scope in tmp_list:
             for error in scope.check_double_def(file_contents, obj_tree):
                 # Check preproc if

fortls/parse_fortran.py
@@ -2,36 +2,45 @@ from __future__ import print_function
 import re
 from fortls.objects import map_keywords, fortran_module, fortran_program, \
     fortran_submodule, fortran_subroutine, fortran_function, fortran_block, \
-    fortran_select, fortran_type, fortran_int, fortran_obj, fortran_meth, fortran_file
+    fortran_select, fortran_type, fortran_int, fortran_obj, fortran_meth, \
+    fortran_do, fortran_where, fortran_if, fortran_file
 #
 USE_REGEX = re.compile(r'[ ]*USE([, ]+INTRINSIC)?[ :]+([a-z0-9_]*)([, ]+ONLY[ :]+)?', re.I)
 INCLUDE_REGEX = re.compile(r'[ ]*INCLUDE[ :]*[\'\"]([^\'\"]*)', re.I)
 SUB_REGEX = re.compile(r'[ ]*(PURE|ELEMENTAL|RECURSIVE)*[ ]*SUBROUTINE[ ]+([a-z0-9_]+)', re.I)
-END_SUB_REGEX = re.compile(r'[ ]*END[ ]*SUBROUTINE', re.I)
+END_SUB_WORD = r'SUBROUTINE'
 FUN_MOD_REGEX = re.compile(r'[ ]*(PURE|ELEMENTAL|RECURSIVE)+', re.I)
 FUN_REGEX = re.compile(r'[ ]*FUNCTION[ ]+([a-z0-9_]+)', re.I)
 RESULT_REGEX = re.compile(r'RESULT[ ]*\(([a-z0-9_]*)\)', re.I)
-END_FUN_REGEX = re.compile(r'[ ]*END[ ]*FUNCTION', re.I)
+END_FUN_WORD = r'FUNCTION'
 MOD_REGEX = re.compile(r'[ ]*MODULE[ ]+([a-z0-9_]+)', re.I)
-END_MOD_REGEX = re.compile(r'[ ]*END[ ]*MODULE', re.I)
+END_MOD_WORD = r'MODULE'
 SUBMOD_REGEX = re.compile(r'[ ]*SUBMODULE[ ]*\(', re.I)
-END_SMOD_REGEX = re.compile(r'[ ]*END[ ]*SUBMODULE', re.I)
+END_SMOD_WORD = r'SUBMODULE'
 BLOCK_REGEX = re.compile(r'[ ]*([a-z_][a-z0-9_]*[ ]*:)?[ ]*BLOCK(?![a-z0-9_])', re.I)
-END_BLOCK_REGEX = re.compile(r'[ ]*END[ ]*BLOCK', re.I)
+END_BLOCK_WORD = r'BLOCK'
+DO_REGEX = re.compile(r'[ ]*(?:[a-z_][a-z0-9_]*[ ]*:)?[ ]*DO([ ]+[0-9]*|$)', re.I)
+END_DO_WORD = r'DO'
+WHERE_REGEX = re.compile(r'[ ]*WHERE[ ]*\(', re.I)
+END_WHERE_WORD = r'WHERE'
+IF_REGEX = re.compile(r'[ ]*IF[ ]*\(', re.I)
+THEN_REGEX = re.compile(r'\)[ ]*THEN$', re.I)
+END_IF_WORD = r'IF'
+END_FIXED_REGEX = re.compile(r'[ ]*([0-9]*)[ ]*CONTINUE', re.I)
 SELECT_REGEX = re.compile(r'[ ]*SELECT[ ]+(CASE|TYPE)[ ]*\(([a-z0-9_=> ]*)', re.I)
 SELECT_TYPE_REGEX = re.compile(r'[ ]*(TYPE|CLASS)[ ]+IS[ ]*\(([a-z0-9_ ]*)', re.I)
 SELECT_DEFAULT_REGEX = re.compile(r'[ ]*CLASS[ ]+DEFAULT', re.I)
-END_SELECT_REGEX = re.compile(r'[ ]*END[ ]*SELECT', re.I)
+END_SELECT_WORD = r'SELECT'
 PROG_REGEX = re.compile(r'[ ]*PROGRAM[ ]+([a-z0-9_]+)', re.I)
-END_PROG_REGEX = re.compile(r'[ ]*END[ ]*PROGRAM', re.I)
+END_PROG_WORD = r'PROGRAM'
 INT_REGEX = re.compile(r'[ ]*(ABSTRACT)?[ ]*INTERFACE[ ]*([a-z0-9_]*)', re.I)
-END_INT_REGEX = re.compile(r'[ ]*END[ ]*INTERFACE', re.I)
-END_GEN_REGEX = re.compile(r'[ ]*END[ ]*$', re.I)
+END_INT_WORD = r'INTERFACE'
+END_WORD_REGEX = re.compile(r'[ ]*END([ ]*[a-z]*|$)', re.I)
 TYPE_DEF_REGEX = re.compile(r'[ ]*(TYPE)[, ]+', re.I)
 EXTENDS_REGEX = re.compile(r'EXTENDS[ ]*\(([a-z0-9_]*)\)', re.I)
 GENERIC_PRO_REGEX = re.compile(r'[ ]*(GENERIC)[ ]*::[ ]*[a-z]', re.I)
 GEN_ASSIGN_REGEX = re.compile(r'(ASSIGNMENT|OPERATOR)\(', re.I)
-END_TYPED_REGEX = re.compile(r'[ ]*END[ ]*TYPE', re.I)
+END_TYPED_WORD = r'TYPE'
 NAT_VAR_REGEX = re.compile(r'[ ]*(INTEGER|REAL|DOUBLE PRECISION|COMPLEX'
                            r'|DOUBLE COMPLEX|CHARACTER|LOGICAL|PROCEDURE'
                            r'|CLASS|TYPE)', re.I)
@@ -244,13 +253,27 @@ def read_sub_def(line, mod_sub=False):
 
 def read_block_def(line):
     block_match = BLOCK_REGEX.match(line)
-    if block_match is None:
-        return None
+    if block_match is not None:
+        name = block_match.group(1)
+        if name is not None:
+            name = name.replace(':', ' ').strip()
+        return 'block', [name]
     #
-    name = block_match.group(1)
-    if name is not None:
-        name = name.replace(':', ' ').strip()
-    return 'block', [name]
+    do_match = DO_REGEX.match(line)
+    if do_match is not None:
+        return 'do', [do_match.group(1).strip()]
+    #
+    where_match = WHERE_REGEX.match(line)
+    if where_match is not None:
+        return 'where', None
+    #
+    if_match = IF_REGEX.match(line)
+    if if_match is not None:
+        line_no_comment = line.split('!')[0].rstrip()
+        then_match = THEN_REGEX.search(line_no_comment)
+        if then_match is not None:
+            return 'if', None
+    return None
 
 
 def read_select_def(line):
@@ -477,7 +500,10 @@ def process_file(file_str, close_open_scopes, path=None, fixed_format=False, deb
     next_line_num = 1
     int_counter = 0
     block_counter = 0
+    do_counter = 0
+    if_counter = 0
     select_counter = 0
+    block_id_stack = []
     # at_eof = False
     next_line = None
     line_ind = 0
@@ -548,22 +574,30 @@ def process_file(file_str, close_open_scopes, path=None, fixed_format=False, deb
             next_line = None
         line = line.rstrip()
         # Test for scope end
-        if file_obj.END_REGEX is not None:
-            match = file_obj.END_REGEX.match(line)
+        if file_obj.END_SCOPE_WORD is not None:
+            line_no_comment = line.split('!')[0]
+            match = END_WORD_REGEX.match(line_no_comment)
             if (match is not None):
+                end_scope_word = match.group(1).strip().upper()
+                if end_scope_word != file_obj.END_SCOPE_WORD:
+                    if file_obj.current_scope.req_named_end() or end_scope_word != '':
+                        file_obj.end_errors.append([line_number, file_obj.current_scope.sline])
                 if (file_obj.current_scope.get_type() == 9) and (file_obj.current_scope.type in (3, 4)):
                     file_obj.end_scope(line_number)
                 file_obj.end_scope(line_number)
                 if(debug):
-                    print('{1} !!! END scope({0})'.format(line_number, line.strip()))
+                    print('{1} !!! END "{2}" scope({0})'.format(line_number, line.strip(), match.group(1).upper()))
                 continue
-            line_no_comment = line.split('!')[0]
-            match = END_GEN_REGEX.match(line_no_comment)
-            if (match is not None):
-                file_obj.end_scope(line_number)
-                if(debug):
-                    print('{1} !!! END scope({0})'.format(line_number, line.strip()))
-                continue
+            if fixed_format and (file_obj.END_SCOPE_WORD in 'DO'):
+                did_close = False
+                while len(block_id_stack) and line_no_comment.lstrip().startswith(block_id_stack[-1]):
+                    file_obj.end_scope(line_number)
+                    block_id_stack.pop()
+                    did_close = True
+                    if(debug):
+                        print('{1} !!! END "DO" scope({0})'.format(line_number, line.strip()))
+                if did_close:
+                    continue
         # Loop through tests
         obj_read = None
         for test in def_tests:
@@ -618,28 +652,28 @@ def process_file(file_str, close_open_scopes, path=None, fixed_format=False, deb
                     print('{1} !!! VARIABLE statement({0})'.format(line_number, line.strip()))
             elif obj_type == 'mod':
                 new_mod = fortran_module(file_obj, line_number, obj, file_obj.enc_scope_name)
-                file_obj.add_scope(new_mod, END_MOD_REGEX)
+                file_obj.add_scope(new_mod, END_MOD_WORD)
                 if(debug):
                     print('{1} !!! MODULE statement({0})'.format(line_number, line.strip()))
             elif obj_type == 'smod':
                 new_smod = fortran_submodule(file_obj, line_number, obj[0], file_obj.enc_scope_name, obj[1])
-                file_obj.add_scope(new_smod, END_SMOD_REGEX)
+                file_obj.add_scope(new_smod, END_SMOD_WORD)
                 if(debug):
                     print('{1} !!! SUBMODULE statement({0})'.format(line_number, line.strip()))
             elif obj_type == 'prog':
                 new_prog = fortran_program(file_obj, line_number, obj, file_obj.enc_scope_name)
-                file_obj.add_scope(new_prog, END_PROG_REGEX)
+                file_obj.add_scope(new_prog, END_PROG_WORD)
                 if(debug):
                     print('{1} !!! PROGRAM statement({0})'.format(line_number, line.strip()))
             elif obj_type == 'sub':
                 new_sub = fortran_subroutine(file_obj, line_number, obj[0], file_obj.enc_scope_name, obj[1], obj[2])
-                file_obj.add_scope(new_sub, END_SUB_REGEX)
+                file_obj.add_scope(new_sub, END_SUB_WORD)
                 if(debug):
                     print('{1} !!! SUBROUTINE statement({0})'.format(line_number, line.strip()))
             elif obj_type == 'fun':
                 new_fun = fortran_function(file_obj, line_number, obj[0], file_obj.enc_scope_name,
                                            obj[1], obj[3], return_type=obj[2][0], result_var=obj[2][1])
-                file_obj.add_scope(new_fun, END_FUN_REGEX)
+                file_obj.add_scope(new_fun, END_FUN_WORD)
                 if obj[2][0] is not None:
                     new_obj = fortran_obj(file_obj, line_number, obj[0], obj[2][0][0], obj[2][0][1],
                                           file_obj.enc_scope_name, None)
@@ -652,9 +686,32 @@ def process_file(file_str, close_open_scopes, path=None, fixed_format=False, deb
                     block_counter += 1
                     name = '#BLOCK{0}'.format(block_counter)
                 new_block = fortran_block(file_obj, line_number, name, file_obj.enc_scope_name)
-                file_obj.add_scope(new_block, END_BLOCK_REGEX, req_container=True)
+                file_obj.add_scope(new_block, END_BLOCK_WORD, req_container=True)
                 if(debug):
                     print('{1} !!! BLOCK statement({0})'.format(line_number, line.strip()))
+            elif obj_type == 'do':
+                do_counter += 1
+                name = '#DO{0}'.format(do_counter)
+                if obj[0] != '':
+                    block_id_stack.append(obj[0])
+                new_do = fortran_do(file_obj, line_number, name, file_obj.enc_scope_name)
+                file_obj.add_scope(new_do, END_DO_WORD, req_container=True)
+                if(debug):
+                    print('{1} !!! DO statement({0})'.format(line_number, line.strip()))
+            elif obj_type == 'where':
+                do_counter += 1
+                name = '#WHERE{0}'.format(do_counter)
+                new_do = fortran_where(file_obj, line_number, name, file_obj.enc_scope_name)
+                file_obj.add_scope(new_do, END_WHERE_WORD, req_container=True)
+                if(debug):
+                    print('{1} !!! WHERE statement({0})'.format(line_number, line.strip()))
+            elif obj_type == 'if':
+                if_counter += 1
+                name = '#IF{0}'.format(if_counter)
+                new_if = fortran_if(file_obj, line_number, name, file_obj.enc_scope_name)
+                file_obj.add_scope(new_if, END_IF_WORD, req_container=True)
+                if(debug):
+                    print('{1} !!! IF statement({0})'.format(line_number, line.strip()))
             elif obj_type == 'select':
                 select_counter += 1
                 name = '#SELECT{0}'.format(select_counter)
@@ -670,7 +727,7 @@ def process_file(file_str, close_open_scopes, path=None, fixed_format=False, deb
                             binding_name = file_obj.current_scope.binding_name
                             bound_var = file_obj.current_scope.bound_var
                 new_select = fortran_select(file_obj, line_number, name, obj, file_obj.enc_scope_name)
-                file_obj.add_scope(new_select, END_SELECT_REGEX, req_container=True)
+                file_obj.add_scope(new_select, END_SELECT_WORD, req_container=True)
                 if binding_name is not None:
                     if obj[0] != 4:
                         bound_var = None
@@ -689,7 +746,7 @@ def process_file(file_str, close_open_scopes, path=None, fixed_format=False, deb
                 new_type = fortran_type(file_obj, line_number, obj[0], modifiers, file_obj.enc_scope_name)
                 if obj[1] is not None:
                     new_type.set_inherit(obj[1])
-                file_obj.add_scope(new_type, END_TYPED_REGEX, req_container=True)
+                file_obj.add_scope(new_type, END_TYPED_WORD, req_container=True)
                 if(debug):
                     print('{1} !!! TYPE statement({0})'.format(line_number, line.strip()))
             elif obj_type == 'int':
@@ -699,13 +756,13 @@ def process_file(file_str, close_open_scopes, path=None, fixed_format=False, deb
                     int_counter += 1
                     name = '#GEN_INT{0}'.format(int_counter)
                 new_int = fortran_int(file_obj, line_number, name, file_obj.enc_scope_name, abstract)
-                file_obj.add_scope(new_int, END_INT_REGEX, req_container=True)
+                file_obj.add_scope(new_int, END_INT_WORD, req_container=True)
                 if(debug):
                     print('{1} !!! INTERFACE statement({0})'.format(line_number, line.strip()))
             elif obj_type == 'gen':
                 name = obj[0]
                 new_int = fortran_int(file_obj, line_number, name, file_obj.enc_scope_name, False)
-                file_obj.add_scope(new_int, END_INT_REGEX, req_container=True)
+                file_obj.add_scope(new_int, END_INT_WORD, req_container=True)
                 for pro_link in obj[1]:
                     file_obj.add_int_member(pro_link)
                 file_obj.end_scope(line_number)

test/test_server.py
@@ -358,7 +358,7 @@ def test_def():
     exp_results = (
         # test_prog.f08
         [0, 0, fixed_path],
-        [20, 20, fixed_path],
+        [19, 19, fixed_path],
         [10, 10, os.path.join(test_dir, "test_prog.f08")],
         [21, 21, free_path],
         [14, 14, free_path],

test/test_source/subdir/test_fixed.f
@@ -12,8 +12,7 @@ c
 c
       myfun = xval
       do 10 i = 1, n
-         myfun = myfun + xval
-   10    continue
+   10    myfun = myfun + xval
       return
 c
       end

test/test_source/test_block.f08
@@ -1,5 +1,5 @@
 SUBROUTINE block_sub()
-INTEGER :: res0
+INTEGER :: res0,i,j
 res0 = 0
 add1 : BLOCK
   INTEGER :: res1
@@ -10,4 +10,14 @@ add1 : BLOCK
     blockVar = res0 + 1
   END BLOCK
 END BLOCK add1
+!
+outer: DO i=1,10
+  DO j=1,i
+    res0=res0+1
+  END DO
+END DO outer
+!
+IF(res0>10)THEN
+  i=res0
+END IF
 END SUBROUTINE block_sub

commit de1841fa1298c86821e4642a854d280880073e21

Fix bug in completion requests introduced in a781239

fortls/langserver.py
@@ -630,7 +630,7 @@ class LangServer:
                 return type
 
         def set_type_mask(def_value):
-            return [def_value if i < 8 else True for i in range(12)]
+            return [def_value if i < 8 else True for i in range(13)]
 
         def get_candidates(scope_list, var_prefix, inc_globals=True, public_only=False, abstract_only=False):
             #