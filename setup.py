ae50877dc44c66bee7e1a87c47343c3692e55c84
William Gurecky <gureckywl@ornl.gov>
2019-03-20 22:16:41 -0400

    adding compdb tpl

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['future','argparse'],
+    install_requires=['future','argparse','compdb'],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
