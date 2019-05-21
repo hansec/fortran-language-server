hansec/fortran-language-server

commit 187da5554088b390283c04610ae3f753600fa7d5

Add support for USE statement renaming in definition/hover requests, ref #109

fortls/objects.py
@@ -79,14 +79,6 @@ def get_keywords(keywords, keyword_info={}):
     return keyword_strings
 
 
-def intersect_lists(l1, l2):
-    tmp_list = []
-    for val1 in l1:
-        if l2.count(val1) > 0:
-            tmp_list.append(val1)
-    return tmp_list
-
-
 def get_paren_substring(test_str):
     i1 = test_str.find('(')
     i2 = test_str.rfind(')')
@@ -96,33 +88,50 @@ def get_paren_substring(test_str):
         return None
 
 
-def get_use_tree(scope, use_dict, obj_tree, only_list=[]):
+def get_use_tree(scope, use_dict, obj_tree, only_list=[], rename_map={}):
+    def intersect_only(new_only, new_map):
+        tmp_list = []
+        tmp_map = rename_map.copy()
+        for val1 in only_list:
+            mapped1 = tmp_map.get(val1, val1)
+            if new_only.count(mapped1) > 0:
+                tmp_list.append(val1)
+                new_rename = new_map.get(mapped1, None)
+                if new_rename is not None:
+                    tmp_map[val1] = new_rename
+            else:
+                tmp_map.pop(val1, None)
+        return tmp_list, tmp_map
     # Add recursively
     for use_stmnt in scope.use:
         use_mod = use_stmnt[0]
         if len(only_list) == 0:
             merged_use_list = use_stmnt[1]
+            merged_rename = use_stmnt[3]
         elif len(use_stmnt[1]) == 0:
             merged_use_list = only_list
+            merged_rename = rename_map
         else:
-            merged_use_list = intersect_lists(only_list, use_stmnt[1])
+            merged_use_list, merged_rename = intersect_only(use_stmnt[1], use_stmnt[3])
             if len(merged_use_list) == 0:
                 continue
         if use_mod in obj_tree:
             if use_mod in use_dict:
-                old_len = len(use_dict[use_mod])
+                old_len = len(use_dict[use_mod][0])
                 if (old_len > 0) and (len(merged_use_list) > 0):
-                    for only_name in use_stmnt[1]:
+                    for only_name in merged_use_list:
                         if use_dict[use_mod].count(only_name) == 0:
-                            use_dict[use_mod].append(only_name)
+                            use_dict[use_mod][0].append(only_name)
                 else:
-                    use_dict[use_mod] = []
+                    use_dict[use_mod] = [[], []]
                 # Skip if we have already visited module with the same only list
-                if old_len == len(use_dict[use_mod]):
+                if old_len == len(use_dict[use_mod][0]):
                     continue
             else:
-                use_dict[use_mod] = merged_use_list
-            use_dict = get_use_tree(obj_tree[use_mod][0], use_dict, obj_tree, merged_use_list)
+                use_dict[use_mod] = [merged_use_list, merged_rename]
+            # Use renaming
+            use_dict = get_use_tree(obj_tree[use_mod][0], use_dict, obj_tree,
+                                    merged_use_list, merged_rename)
     return use_dict
 
 
@@ -148,16 +157,19 @@ def find_in_scope(scope, var_name, obj_tree, interface=False, local_only=False):
     # Setup USE search
     use_dict = get_use_tree(scope, {}, obj_tree)
     # Look in found use modules
-    for use_mod, only_list in use_dict.items():
+    for use_mod, only_info in use_dict.items():
         use_scope = obj_tree[use_mod][0]
         # Module name is request
         if use_mod.lower() == var_name_lower:
             return use_scope
         # Filter children by only_list
+        only_list = only_info[0]
+        only_renames = only_info[1]
         if len(only_list) > 0:
             if var_name_lower not in only_list:
                 continue
-        tmp_var = check_scope(use_scope, var_name_lower, filter_public=True)
+        mod_name = only_renames.get(var_name_lower, var_name_lower)
+        tmp_var = check_scope(use_scope, mod_name, filter_public=True)
         if tmp_var is not None:
             return tmp_var
     # Only search local and imported names for interfaces
@@ -495,11 +507,10 @@ class fortran_scope(fortran_obj):
         else:
             self.FQSN = self.name.lower()
 
-    def add_use(self, use_mod, line_number, only_list=[]):
-        lower_only = []
-        for only in only_list:
-            lower_only.append(only.lower())
-        self.use.append([use_mod.lower(), lower_only, line_number])
+    def add_use(self, use_mod, line_number, only_list=[], rename_map={}):
+        lower_only = [only.lower() for only in only_list]
+        rename_lower = {key.lower(): value.lower() for key, value in rename_map.items()}
+        self.use.append([use_mod.lower(), lower_only, line_number, rename_lower])
 
     def set_inherit(self, inherit_type):
         self.inherit = inherit_type
@@ -1744,10 +1755,10 @@ class fortran_ast:
     def add_public(self, name):
         self.public_list.append(self.enc_scope_name+'::'+name)
 
-    def add_use(self, mod_word, line_number, only_list):
+    def add_use(self, mod_word, line_number, only_list=[], rename_map={}):
         if self.current_scope is None:
             self.create_none_scope()
-        self.current_scope.add_use(mod_word, line_number, only_list)
+        self.current_scope.add_use(mod_word, line_number, only_list, rename_map)
 
     def add_include(self, path, line_number):
         self.include_stmnts.append([line_number, path, []])

fortls/parse_fortran.py
@@ -655,9 +655,15 @@ def read_use_stmt(line):
         trailing_line = line[use_match.end(0):].lower()
         use_mod = use_match.group(2)
         only_list = []
+        rename_map = {}
         if use_match.group(3) is not None:
-            only_list = [only_stmt.split('=>')[0].strip() for only_stmt in trailing_line.split(',')]
-        return 'use', [use_mod, only_list]
+            for only_stmt in trailing_line.split(','):
+                only_split = only_stmt.split('=>')
+                only_name = only_split[0].strip()
+                only_list.append(only_name)
+                if len(only_split) == 2:
+                    rename_map[only_name] = only_split[1].strip()
+        return 'use', [use_mod, only_list, rename_map]
 
 
 def read_inc_stmt(line):
@@ -1573,7 +1579,7 @@ def process_file(file_obj, close_open_scopes, debug=False, pp_defs={}):
                 if(debug):
                     print('{1} !!! INTERFACE-PRO statement({0})'.format(line_number, line.strip()))
             elif obj_type == 'use':
-                file_ast.add_use(obj[0], line_number, obj[1])
+                file_ast.add_use(obj[0], line_number, obj[1], obj[2])
                 if(debug):
                     print('{1} !!! USE statement({0})'.format(line_number, line.strip()))
             elif obj_type == 'import':

test/test_server.py
@@ -166,6 +166,7 @@ def test_workspace_symbols():
             ["test_inherit", 2, 0],
             ["test_mod", 2, 0],
             ["test_program", 2, 0],
+            ["test_rename_sub", 6, 9],
             ["test_select", 2, 0],
             ["test_select_sub", 6, 16],
             ["test_sig_Sub", 6, 67],
@@ -350,6 +351,9 @@ def test_def():
     string += def_request(file_path, 10, 2)
     file_path = os.path.join(test_dir, "subdir", "test_inc2.f90")
     string += def_request(file_path, 3, 2)
+    file_path = os.path.join(test_dir, "subdir", "test_rename.F90")
+    string += def_request(file_path, 13, 5)
+    string += def_request(file_path, 14, 5)
     errcode, results = run_request(string)
     assert errcode == 0
     #
@@ -369,7 +373,10 @@ def test_def():
         # test_inc.f90
         [0, 0, os.path.join(test_dir, "subdir", "test_inc2.f90")],
         # subdir/test_inc2.f90
-        [4, 4, os.path.join(test_dir, "test_inc.f90")]
+        [4, 4, os.path.join(test_dir, "test_inc.f90")],
+        # subdir/test_rename.F90
+        [6, 6, os.path.join(test_dir, "subdir", "test_rename.F90")],
+        [1, 1, os.path.join(test_dir, "subdir", "test_rename.F90")]
     )
     assert len(exp_results)+1 == len(results)
     for i in range(len(exp_results)):

commit 0d2bfb5db3d20350b4080f81fabe4ef6e677c38e

Fix bugs introduced in 187da55

fortls/langserver.py
@@ -430,8 +430,9 @@ class LangServer:
                 if not no_use:
                     use_dict = get_use_tree(scope, use_dict, self.obj_tree)
             # Look in found use modules
-            for use_mod, only_list in use_dict.items():
+            for use_mod, only_info in use_dict.items():
                 scope = self.obj_tree[use_mod][0]
+                only_list = only_info[0]
                 var_list += child_candidates(scope, only_list, req_abstract=abstract_only)
             # Add globals
             if inc_globals:

fortls/objects.py
@@ -106,11 +106,11 @@ def get_use_tree(scope, use_dict, obj_tree, only_list=[], rename_map={}):
     for use_stmnt in scope.use:
         use_mod = use_stmnt[0]
         if len(only_list) == 0:
-            merged_use_list = use_stmnt[1]
-            merged_rename = use_stmnt[3]
+            merged_use_list = use_stmnt[1][:]
+            merged_rename = use_stmnt[3].copy()
         elif len(use_stmnt[1]) == 0:
-            merged_use_list = only_list
-            merged_rename = rename_map
+            merged_use_list = only_list[:]
+            merged_rename = rename_map.copy()
         else:
             merged_use_list, merged_rename = intersect_only(use_stmnt[1], use_stmnt[3])
             if len(merged_use_list) == 0:
@@ -123,7 +123,7 @@ def get_use_tree(scope, use_dict, obj_tree, only_list=[], rename_map={}):
                         if use_dict[use_mod].count(only_name) == 0:
                             use_dict[use_mod][0].append(only_name)
                 else:
-                    use_dict[use_mod] = [[], []]
+                    use_dict[use_mod] = [[], {}]
                 # Skip if we have already visited module with the same only list
                 if old_len == len(use_dict[use_mod][0]):
                     continue