785b66fe2ff64ec04c45b3c558159b6aee4ce129
Chris Hansen <hansec@uw.edu>
2019-05-21 13:08:04 -0400

    Add Fortran source file for tests added in commit 187da55

---
+++ test/test_source/subdir/test_rename.F90
@@ -0,0 +1,16 @@
+module rename_mod1
+integer :: var1
+end module rename_mod1
+!
+module rename_mod2
+use rename_mod1, only: renamed_var1 => var1
+integer :: originalname
+end module rename_mod2
+!
+subroutine test_rename_sub()
+use rename_mod2, only : localname => originalname, renamed_var2 => renamed_var1
+implicit none
+!
+localname = 4
+renamed_var2 = 4    
+end subroutine test_rename_sub
