hansec/fortran-language-server

commit 5d759bb12728dd072d69b72993ef74e41505990d

Add support for specifying the language name returned for hover requests

fortls/__init__.py
@@ -69,6 +69,10 @@ def main():
         help="Show signature information in hover for argument (also enables '--variable_hover')"
     )
     parser.add_argument(
+        '--hover_language', type=str, default=None,
+        help="Language used for responses to hover requests (for editor syntax highlighting)"
+    )
+    parser.add_argument(
         '--preserve_keyword_order', action="store_true",
         help="Display variable keywords information in original order (default: sort to consistent ordering)"
     )
@@ -181,6 +185,7 @@ def main():
         "use_signature_help": args.use_signature_help,
         "variable_hover": (args.variable_hover or args.hover_signature),
         "hover_signature": args.hover_signature,
+        "hover_language": args.hover_language,
         "sort_keywords": (not args.preserve_keyword_order),
         "enable_code_actions": (args.enable_code_actions or args.debug_actions),
         "max_line_length": args.max_line_length,

fortls/langserver.py
@@ -93,6 +93,7 @@ class LangServer:
         self.use_signature_help = settings.get("use_signature_help", False)
         self.variable_hover = settings.get("variable_hover", False)
         self.hover_signature = settings.get("hover_signature", False)
+        self.hover_language = settings.get("hover_language", "fortran90")
         self.sort_keywords = settings.get("sort_keywords", True)
         self.enable_code_actions = settings.get("enable_code_actions", False)
         self.max_line_length = settings.get("max_line_length", -1)
@@ -952,7 +953,7 @@ class LangServer:
         def create_hover(string, highlight):
             if highlight:
                 return {
-                    "language": "fortran90",
+                    "language": self.hover_language,
                     "value": string
                 }
             else:

commit 6a36d621c1920b22d5f3bf02abe1145afd6859ac

Fix bug with hover requests introduced in 5d759bb, fixes #159

fortls/__init__.py
@@ -185,12 +185,13 @@ def main():
         "use_signature_help": args.use_signature_help,
         "variable_hover": (args.variable_hover or args.hover_signature),
         "hover_signature": args.hover_signature,
-        "hover_language": args.hover_language,
         "sort_keywords": (not args.preserve_keyword_order),
         "enable_code_actions": (args.enable_code_actions or args.debug_actions),
         "max_line_length": args.max_line_length,
         "max_comment_line_length": args.max_comment_line_length
     }
+    if args.hover_language is not None:
+        settings["hover_language"] = args.hover_language
     #
     if args.debug_parser:
         if args.debug_filepath is None: